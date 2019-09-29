3b262711937854af29fa5f2823fbac5f7c4b0da0
Nalini <nalini.hogwarts@gmail.com>
2019-09-29 15:07:01 -0400

    removed LongestPalindrome folder

---
+++ LngstSbStrngPalidrome/LngstSbStrngPalindrome.cpp
@@ -0,0 +1,6 @@
+#include "lngstsbstrngpalindrome.h"
+
+LngstSbStrngPalindrome::LngstSbStrngPalindrome()
+{
+
+}
---
+++ LngstSbStrngPalidrome/LngstSbStrngPalindrome.h
@@ -0,0 +1,11 @@
+#ifndef LNGSTSBSTRNGPALINDROME_H
+#define LNGSTSBSTRNGPALINDROME_H
+
+
+class LngstSbStrngPalindrome
+{
+public:
+    LngstSbStrngPalindrome();
+};
+
+#endif // LNGSTSBSTRNGPALINDROME_H
